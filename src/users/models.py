6029adffbc5250bc039259a8930030e6c83073c4
Miguel Alexander Ontiveros Canul <miguel.ontiveros@gulf.mx>
2025-08-11 07:53:56 -0500

    feat:initial commit

---
+++ src/users/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    first_name: str
+    last_name: str
+
+
+class PasswordChange(BaseModel):
+    current_password: str
+    new_password: str
+    new_password_confirm: str
