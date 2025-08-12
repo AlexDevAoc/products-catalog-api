00a48f2272f9a1d35490a7ce05cd3d558d0ad3b7
Miguel Ontiveros <miguel_isco93@hotmail.com>
2025-08-11 20:02:48 -0500

    db: add env variable for db connection

--- src/database/core.py
+++ src/database/core.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-""" You can add a DATABASE_URL environment variable to your .env file """
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
