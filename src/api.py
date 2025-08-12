494d64d9c796996f28053a3f43c985a27034a845
Miguel Ontiveros <miguel_isco93@hotmail.com>
2025-08-11 20:22:16 -0500

    feat: add router for role, permission, user_change_log module

--- src/api.py
+++ src/api.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from src.auth.controller import router as auth_router
 from src.users.controller import router as users_router
+from src.roles.controller import router as roles_router
+from src.permissions.controller import router as permissions_router
+from src.user_change_logs.controller import router as user_change_logs_router
 
 def register_routes(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(users_router)
+    app.include_router(roles_router)
+    app.include_router(permissions_router)
+    app.include_router(user_change_logs_router)
